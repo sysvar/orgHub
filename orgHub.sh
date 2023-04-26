c774c519b532e241aba2f0b1124cb39d5c82bd96
sysvar <sysvar@users.noreply.github.com>
2023-04-26 22:50:03 +0100

    Adding Cockpit Metric Package

--- orgHub.sh
+++ orgHub.sh
@@ -356,7 +356,7 @@ Origins = https://somedomain1.com https://somedomain2.com:9090
 EOT
 
     apt update -qq >/dev/null 2>&1
-    pkgs='cockpit cockpit-podman cockpit-machines'
+    pkgs='cockpit cockpit-pcp cockpit-podman cockpit-machines'
     for pkg in $pkgs; do
       status="$(dpkg-query -W --showformat='${db:Status-Status}' "$pkg" 2>&1)"
       if [ ! $? = 0 ] || [ ! "$status" = installed ]; then
